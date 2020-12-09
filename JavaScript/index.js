8ba23e38441fdb700994d77a94356486648c7474
teri-akimoto <nguyenhlan96@gmail.com>
2020-12-09 14:50:57 -0800

    fixed scroll behaviour

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,21 @@
+// ------------------------------------------------------------------------------
+//Scroll Behaviour --------------------------------------------------------------
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".section")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) { 
+            $(tag).addClass("visible")
+        }
+        else { 
+            $(tag).removeClass("visible")
+        }
+    }
+})
 
 $(".russian-disabled, .russian").click(function() {
     console.log("russian");
