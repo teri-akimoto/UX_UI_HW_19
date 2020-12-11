b85ffec00b4eef6489c7813f564ea885387796b4
teri-akimoto <nguyenhlan96@gmail.com>
2020-12-10 17:17:59 -0800

    appear on scroll removed for fixes

--- JavaScript/Endangered-Species.js
+++ JavaScript/Endangered-Species.js
@@ -1,22 +1,3 @@
-// ------------------------------------------------------------------------------
-//Scroll Behaviour --------------------------------------------------------------
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop()
-    var pageBottom = pageTop + $(window).height()
-    var tags = $(".section")
-
-    for (var i = 0; i < tags.length; i++) {
-        var tag = tags[i]
-
-        if ($(tag).position().top < pageBottom) { 
-            $(tag).addClass("visible")
-        }
-        else { 
-            $(tag).removeClass("visible")
-        }
-    }
-})
-
 $("#summary-tab, #ux-research-tab, #definition-tab, #prototyping-tab, #final-tab, #reflection-tab").click(function(e) {
     e.preventDefault();
     var offset = 0;
